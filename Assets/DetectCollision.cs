559633e02ff8f86eefdef7f2147c2d36f6cfdd1e
Nafsin Rahman <33981434+Nafsin@users.noreply.github.com>
2020-09-21 16:25:37 +0600

    Last change
    Last change

---
+++ Assets/DetectCollision.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollision : MonoBehaviour {
+	public play movement;
+
+	void OnCollisionEnter(Collision DetectCollision)
+	{
+		if (DetectCollision.collider.tag == "obstacles") 
+		{
+			Debug.Log ("Woops! The cube collided with something!!!!!");
+			movement.enabled = false;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
